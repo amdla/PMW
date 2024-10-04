221ac1d871a302c3ac939bdf92dff994b984ca66
Maciej Karabin <maciej.m.karabin@gmail.com>
2024-10-04 23:44:43 +0200

    added tasks 1-3

---
+++ lab1/RazorApp/Pages/Form.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorApp.Pages;
+
+public class Form : PageModel
+{
+    [BindProperty] public string Name { get; set; }
+
+    [BindProperty] public string Surname { get; set; }
+
+    [TempData] public string SuccessMessage { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    // Przekazanie danych z logiki aplikacji do widoku poprzed 'TempData'
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            SuccessMessage = "Form submitted successfully!";
+            return Page();
+        }
+
+        return Page();
+    }
+}
---
+++ lab1/RazorApp/Pages/Register.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorApp.Pages;
+
+public class Register : PageModel
+{
+    [BindProperty]
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; }
+
+    [BindProperty]
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+        ErrorMessage =
+            "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character")]
+    public string Password { get; set; }
+
+    [TempData] public string SuccessMessage { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            SuccessMessage = "Form submitted successfully!";
+            return RedirectToPage();
+        }
+
+        return Page();
+    }
+}
