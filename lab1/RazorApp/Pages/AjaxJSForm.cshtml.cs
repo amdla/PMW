8bbc0f618660f11a6911519fdf8040bbde6074c4
Maciej Karabin <maciej.m.karabin@gmail.com>
2024-10-05 12:26:50 +0200

    added task 4 - form with AJAX and JS

---
+++ lab1/RazorApp/Pages/AjaxJSForm.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorApp.Pages;
+
+public class AjaxJSForm : PageModel
+{
+    [BindProperty] public string Name { get; set; }
+
+    [BindProperty] public string Surname { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            // if everything works correctly, send JSON response for Ajax request
+            return new JsonResult(new
+            {
+                success = true,
+                message = "Form submitted successfully!",
+                name = Name,
+                surname = Surname,
+            });
+        }
+
+        return new JsonResult(new { success = false });
+    }
+}
