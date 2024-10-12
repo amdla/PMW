595e13ef6b758b48fdc292594404aa12d32c178b
amdla <maciej.m.karabin@gmail.com>
2024-10-12 20:14:49 +0200

    added files necessary for TODO app, added routing and paths leading to the TODO part

---
+++ lab2/zad/zad/Controllers/ToDoAppController.cs
@@ -0,0 +1,23 @@
+﻿namespace zad.Controllers;
+
+using Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class ToDoAppController : Controller
+{
+    [HttpGet]
+    public IActionResult ToDoApp()
+    {
+        return View(new ToDoAppModel());
+    }
+
+    [HttpPost]
+    public IActionResult ToDoApp(ToDoAppModel model)
+    {
+        if (ModelState.IsValid)
+        {
+        }
+
+        return View(model);
+    }
+}
---
+++ lab2/zad/zad/Models/ToDoAppModel.cs
@@ -0,0 +1,5 @@
+﻿namespace zad.Models;
+
+public class ToDoAppModel
+{
+}
