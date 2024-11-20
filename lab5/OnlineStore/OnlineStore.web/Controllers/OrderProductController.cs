7298c8c8274067488f965d9b56a35cebbff07d92
Maciej Karabin <maciej.m.karabin@gmail.com>
2024-11-21 00:37:09 +0100

    fixed lab4, updated lab5

---
+++ lab5/OnlineStore/OnlineStore.web/Controllers/OrderProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.web.Controllers;
+
+public class OrderProductController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ lab5/OnlineStore/OnlineStore.web/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Data;
+using OnlineStore.Models;
+
+namespace OnlineStore.Web.Controllers;
+
+public class ProductsController(AppDbContext context) : Controller
+{
+    public IActionResult Index()
+    {
+        var products = context.Products.ToList();
+        return View(products);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(product);
+    }
+}
