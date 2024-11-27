5bfd8315167eb8015ebf39598d9e0b02e9f5a8a8
Maciej Karabin <maciej.m.karabin@gmail.com>
2024-11-27 15:33:11 +0100

    lab6 init

---
+++ lab6/ToDoApp/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using ToDoApp.Services;
+using ToDoApp.Views;
+using ToDoApp.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace ToDoApp
+{
+
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                });
+
+            // Rejestracja serwisu
+            builder.Services.AddSingleton<IToDoService, ToDoService>();
+
+            // Rejestracja ViewModelu
+            builder.Services.AddSingleton<ToDoViewModel>();
+
+            // Rejestracja widoku MainPage
+            builder.Services.AddSingleton<UsersPage>();
+
+            return builder.Build();
+        }
+    }
+}
+
