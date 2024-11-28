acb1dc399b96e8534b93f88bd56b570685727422
Maciej Karabin <maciej.m.karabin@gmail.com>
2024-11-28 22:01:41 +0100

    moze dziala

---
+++ lab6/ToDoApp/Models/ToDoItemCategory.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace ToDoApp.Models
+{
+    public class ToDoItemCategory
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public int ToDoItemId { get; set; }   // Klucz obcy do ToDoItem
+        public int CategoryId { get; set; }    // Klucz obcy do Category
+    }
+}
